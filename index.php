1f42ad2f341bfa1f48fb0c6f1de2430583e5bf69
Soib <r.soib1996@gmail.com>
2019-06-20 13:32:11 +0300

    edit index file and create Router

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php 
 	
-	echo "ok";
+	//FRONT CONTROLLER
+
+
+	// 1. All settings
+	
+	ini_set('display_errors', 1);
+	error_reporting(E_ALL);
+
+	// 2.Connected files system
+
+	define('ROOT', dirname(__FILE__));
+	include_once(ROOT.'/components/Router.php');
+
+	// 3.Setting connection database
+
+	// 4.Call Router
 
 ?>
