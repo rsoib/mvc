0b82b725cae26b4e1aa6880470c04dc10af3f5c1
Soib <r.soib1996@gmail.com>
2019-06-20 13:35:50 +0300

    edit class Router

---
+++ components/Router.php
@@ -0,0 +1,21 @@
+<?php
+	
+class Router
+{
+
+	//Это массив в котором хранится маршруты
+	
+	private $routes
+
+	public function __construct()
+	{
+
+	}
+
+	public function run()
+	{
+		
+	}
+}
+
+?>
