f9f9fb51cd4960f075acc2bf74a297ad5160254b
Soib <r.soib1996@gmail.com>
2019-07-03 13:35:07 +0300

    create model News

---
+++ models/News.php
@@ -0,0 +1,29 @@
+<?php 
+	
+class News
+{
+
+		/* 
+			Получение одной новости
+		*/
+
+		public static function getNewsItemById($id)
+		{
+			// Запрос к БД
+		} 
+
+
+		/*
+			Получение все новости
+		*/
+
+		public static function getNewsList()
+		{
+			// Запрос к БД
+
+		} 
+}
+
+
+
+?>
