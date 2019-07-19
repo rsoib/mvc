fa771431a6ff366e7b915a18e7ecae51f8dc1587
Soib <r.soib1996@gmail.com>
2019-07-19 13:40:31 +0300

    project ready 95%

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,9 @@
 <?php 
 	
 	return [
-		'' => 'index/show',
 		'news/([0-9]+)' => 'news/view/$1', // NewsController/view  method
 		'news' => 'news/index' // NewsCintroller/index method
+		'' => 'index/show', //IndexController/show method --main page --
 	]
 
 ?>
